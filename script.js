9b8f18333a79e251a5464b820009125184eb380d
Harry Nguyen <vunguyen.theophys@gmail.com>
2024-09-02 14:25:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form submission alert for demo purposes
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+});
