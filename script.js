d3948cd5176ba963649e63b92af8b6a34000d7f3
Harry Nguyen <vunguyen.theophys@gmail.com>
2024-09-02 14:46:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     event.preventDefault();
     alert('Thank you for your message!');
 });
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+        $('body').addClass('scrolled');
+    } else {
+        $('body').removeClass('scrolled');
+    }
+});
